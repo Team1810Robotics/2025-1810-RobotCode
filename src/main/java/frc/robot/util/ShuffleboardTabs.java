f6b5d11a0d28b40358a7619f8a95a5d359e02296
SBowling4 <bowlingmsam@gmail.com>
2025-10-01 18:37:43 -0500

    Added PoseEstimation to vision

---
+++ src/main/java/frc/robot/util/ShuffleboardTabs.java
@@ -0,0 +1,15 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class ShuffleboardTabs {
+    public static final ShuffleboardTab TELEOPERATED = Shuffleboard.getTab("TELEOPERATED");
+    public static final ShuffleboardTab AUTONOMOUS = Shuffleboard.getTab("AUTONOMOUS");
+    public static final ShuffleboardTab INTAKE = Shuffleboard.getTab("INTAKE");
+    public static final ShuffleboardTab PITCH = Shuffleboard.getTab("PITCH");
+    public static final ShuffleboardTab ROLL = Shuffleboard.getTab("ROLL");
+    public static final ShuffleboardTab EXTENDER = Shuffleboard.getTab("EXTENDER");
+    public static final ShuffleboardTab ARM = Shuffleboard.getTab("ARM");
+    public static final ShuffleboardTab VISION = Shuffleboard.getTab("VISION");
+}
